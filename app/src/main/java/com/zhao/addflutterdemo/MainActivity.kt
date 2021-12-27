be807b9a7c4f47e1f55d561a499497c3caf42e13
zhao <754251092@qq.com>
2021-12-27 16:07:38 +0800

    引入flutter模块

--- app/src/main/java/com/zhao/addflutterdemo/MainActivity.kt
+++ app/src/main/java/com/zhao/addflutterdemo/MainActivity.kt
@@ -2,11 +2,19 @@ package com.zhao.addflutterdemo
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import io.flutter.embedding.android.FlutterActivity
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        findViewById<View>(R.id.tv_hi).setOnClickListener(View.OnClickListener {
+            startActivity(
+                FlutterActivity.createDefaultIntent(this)
+            )
+        })
+
     }
 }
